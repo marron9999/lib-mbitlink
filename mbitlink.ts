e5f71b43843cc6c00f973d9b7a3144c52f3e8548
Hiroyuki Inaba <46040144+marron9999@users.noreply.github.com>
2021-03-28 11:19:59 +0900

    Update mbitlink.ts

--- mbitlink.ts
+++ mbitlink.ts
@@ -83,7 +83,7 @@ export function start(sleep : number = 50) {
 /**
  * dispatch message
  */
-//% blockId=mbitlink_start block="Dispatch message"
+//% blockId=mbitlink_dispatch block="Dispatch message"
 //% weight=95
 export function dispatch() {
     if (_received.length > 0) {
